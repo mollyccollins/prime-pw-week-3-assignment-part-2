ec3baf503263698e7f4cd1f2d3f737791ce8e7a2
Molly Collins <molly.c.collins@gmail.com>
2022-04-26 10:27:46 -0500

    resubmit

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -14,28 +14,32 @@ for (let i=0; i<4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 
-for(let i=3; i<6; i++) {
+for(let i=0; i<6; i++) {
   console.log(i);
 }
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 
-for(let i=0; i<6; i++) {
-  console.log('count from 3 to 5');
+console.log('count from 3 to 5');
+
+for(let i=3; i<6; i++) {
+  console.log(i);
 }
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
-
-let evenNumbers=[]
-for( i=0; i<evenNumbers*2; i++) {
-  console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):' , evenNumbers);
-
+console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):') ;
+console.log('count from 2 to 10');
+for (let i=2; i<=10; i+=2) {  
+  console.log(i)
 }
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
+for (let i=5; i>-1; i--){
+  console.log(i)
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -51,11 +55,25 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let star = 0
+while (star<stars.length) {
+  console.log(stars[star])
+  star++
+}
+
 
 console.log([stars]);
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+let count = 0
+while (count<=5) {
+  console.log(count)
+  count++
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let newCount = 10
+while (i >= 5);
+    console.log(i);
+    i--;
